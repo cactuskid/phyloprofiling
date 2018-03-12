b53716d23949c2f4db7b91d48921636c11cacdb0
cactuskid <cactuskid13@gmail.com>
2018-03-12 11:40:26 +0100

    hash serialization

---
+++ pyhamALL/profileGen.py
@@ -0,0 +1,60 @@
+#use dask and pyham to create a big sparse matrix for each type of evolutionary event
+import pyham
+import dask
+import datasketch
+import ete3
+import sparse
+
+
+
+def generateTaxaIndex(newick):
+	#use this to generate an index for the global taxonomic tree for all of OMA
+	t =ete3.Tree( newick)
+	taxaIndex = {}
+	for i,n in enumerate(t.traverse):
+		taxaIndexReverse[i] = node.name
+		taxaIndex[node.name] = i
+	return taxaIndex, taxaIndexReverse
+
+def pyhamtoTree(hamOBJ):
+	#use pyham to get all evolutionary events from a pyham object
+	#turn into an array and a hash
+	tp = pyham.TreeProfile(hamOBJ)
+	tree = tp.compute_tree_profile_full()
+	return tree
+
+def Tree2Hashes(eteobj):
+	#turn each tree into a minhash object
+	#serialize and store as array
+
+	eventdict = { 'presence':[] , 'gain':[] , 'loss':[] , 'duplication':[]}
+	
+	for node in tree.traverse():
+		node.
+
+	hashes= []
+	for array in eventdict:
+		#generate minhash
+		eventdict[array] = set(eventdict[array])
+		m1 = MinHash(num_perm=128)
+		for element in eventdict[array]:
+			m1.update(element)
+		m1 = LeanMinHash(m1)
+		buf = bytearray(m1.bytesize())
+		m1.serialize(buf)
+		hashes.apped([m1.serialize()])
+	hashmat = np.vstack(hashes)
+	return hashmat
+
+def Tree2mat(eteobj, taxaIndex):
+	#use partials to configure the taxa index
+	#turn each tree into a sparse matrix with 4 rows
+	rowdict={ 'presence':0 , 'gain':1 , 'loss':2 , 'duplication':3}
+	matrix = sparse((len(rowdict), len(taxaIndex) ))
+	for node in tree.traverse():
+		matrix[ rowdict[] , taxaIndex[] ] = 1
+
+
+
+
+
