e8b14293c79f27ae4455804056aef9f35efba7d4
cactuskid <cactuskid13@gmail.com>
2018-03-12 16:20:25 +0100

    cleaning up

--- pyhamALL/pyhamAll.py
+++ pyhamALL/pyhamAll.py
@@ -11,20 +11,37 @@ import tempfile
 import functools
 import config
 
-#species_tree = pyham.utils.get_newick_string(working_dir + "speciestree.nwk", type="nwk")
 with open( config.working_dir + "speciestree.nwk" , 'r') as treefile:
     species_tree = treefile.read()
+t = Tree(species_tree, format =1 )
+print(t)
+#rewrite IDs and reload tree
+#
+species_tree = pyham.utils.get_newick_string(working_dir + "speciestree_hack.nwk", type="nwk")
 
 
+#open up OMA
 h5file = open_file(omadir + 'OmaServer.h5', mode="r") 
 #setup db objects
 dbObj = db.Database(h5file)
 omaIdObj = db.OmaIdMapper(dbObj)
 
+#load Fams
 
-t = Tree(species_tree, format =1 )
-print(t)
-species_tree = pyham.utils.get_newick_string(working_dir + "speciestree_hack.nwk", type="nwk")
+#load orthoxml
 
+#map the IDhack function to loaded orthoxml
+
+#generate pyham objects
+
+#generate minhash
+
+#generate matrix rows
+
+#comile LSH from serialized minhashes
+
+#save lsh objects
+
+#DONE
 
 
