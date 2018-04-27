d0fb3f0c791599020624395fdfff0fc11f71661d
cactuskid <cactuskid13@gmail.com>
2018-04-27 10:56:54 +0200

    mp fine tuning

--- pyhamALL/pyhamAll.py
+++ pyhamALL/pyhamAll.py
@@ -51,8 +51,18 @@ if __name__ == '__main__':
 	for dataset_name in dataset_names:
 			if dataset_name not in list(h5hashes.keys()):
 				dataset = h5hashes.create_dataset(dataset_name, (chunksize,0), maxshape=(None, None), dtype = 'int32')
-	
 
+
+	dsets = {}
+	matrixdsets = {}
+	for dataset_name in list(h5hashes.keys()):
+		dsets[dataset_name] = h5hashes[dataset_name]
+		
+	for dataset_name in list(h5matrix.h5f.keys()):
+		if dataset_name == 'fam':
+			matrixdsets[dataset_name] = h5matrix.h5f[dataset_name]
+		else:
+			matrixdsets[dataset_name] = h5matrix[dataset_name]
 
 	# corrects species tree and replacement dictionary for orthoXML files
 	dic, tree = format_files.create_species_tree(h5OMA, omaIdObj)
@@ -82,63 +92,57 @@ if __name__ == '__main__':
 		
 
 		print('init cluster')
-		cluster = LocalCluster(n_workers=25)
+		cluster = LocalCluster(n_workers=int(mp.cpu_count()/2))
 
 		print('init client')
 		c = Client(cluster)
-
+		
 		print('config functions')
 		HAMPIPELINE = functools.partial( pyhamPipeline.runpyham , species_tree=tree )
 		HASHPIPEline = functools.partial( profileGen.DFTree2Hashes  )
 		ROWPIPELINE = functools.partial( profileGen.Tree2mat , taxaIndex = taxaIndex)
+		
 		print('start!')
-
 		fams = {}
+		
 		for i,fam in enumerate( pyhamPipeline.yieldFamilies(h5OMA,startfam)):
 			fams[fam] = { 'ortho':pyhamPipeline.readortho( fam ,   dbObj= dbObj , species_tree=tree , replacement_dic= dic)}
-			
 			if len(fams)>chunksize:
 				
+				print(time.clock()-start)
+
 				pddf = pd.DataFrame.from_dict(fams, orient= 'index' )	
 				pddf['fams'] = pddf.index
 				df = ddf.from_pandas(pddf , chunksize = 10 )
 				df['tree'] = df[['fams','ortho']].apply( HAMPIPELINE , axis =1 ,  meta=pd.Series(dtype=object ) ).compute()
 				df['hashes'] = df[['fams','tree']].apply( HASHPIPEline , axis =1 ,  meta=pd.Series(dtype=object) ).compute()
 				df['rows'] = df['tree'].apply( ROWPIPELINE, meta=pd.Series(dtype=object ) ).compute()
-				
-				for dico in df['hashes'].compute().to_dict():
-					for fam in dico['dict']:
-						lsh.insert( fam , dico['dict'][fam])
-					for hashval in dico['hashes']:		
-				
-				with open(config.datadir + 'lsh.pkl' , 'wb') as lshout:
+				hashes = df['hashes'].compute().to_dict()
+				for fam in hashes:
+					for famhashname in hashes[fam]['dict']:
+						lsh.insert( famhashname , hashes[fam]['dict'][famhashname])
+				with open(config.datadir + 'newlsh.pkl' , 'wb') as lshout:
 					pickle.dump(lsh, lshout, -1)
 				
-				matrixdsets['hogmatrix'].append(vstack(df['rows'].compute().to_list()))
-						
-				for col in df['hashes']:
-					if dataset == 'fam':
-						dsets[dataset].resize((startfam+ chunksize+1, 1 ))
-					else: 
-						dsets[dataset].resize((startfam + chunksize+1, np.asarray(hashesDic[dataset]).shape[0] ))
-			
-				for dataset in dsets:
-					if dataset == 'fam':
-						dsets[dataset].resize((startfam + i+chunksize+1, 1 ))
-					else: 
-						dsets[dataset].resize((startfam + i+chunksize, np.asarray(hashesDic[dataset]).shape[0] ))
+				#matrixdsets['rows'].append(vstack(df['rows'].compute().to_list()))
 				
-				matrixdsets['hogmatrix'].append(vstack(rows))
-				rows= []
-			for dataset in dsets:
-				if dataset == 'fam':
-					dsets[dataset][i,:] = fam
-				else:
-					dsets[dataset][i,:] = hashesDic[dataset]
+				for fam in hashes:
+					if fam:
+						hashvals = hashes[fam]['hashes']
+						print(fam)
+						for i,event in enumerate(hashvals):
+							dataset = dataset_names[i]
+							if len(dsets[dataset])< fam+10:
+								dsets[dataset].resize( (len(dsets[dataset]) +chunksize , len(hashvals[event]) ) )
+							dsets[dataset][fam,:] = hashvals[event]
 				print(time.clock()-start)
-				
 				fams = {}
-				break
+				
+	
+	print('DONE')
+
+
+				
 				 
 
 
@@ -158,16 +162,6 @@ if __name__ == '__main__':
 				else:
 					dataset = h5matrix.h5f.create_dataset(dataset_name, (chunksize,0), maxshape=(None, None), dtype = 'int32')
 
-		dsets = {}
-		matrixdsets = {}
-		for dataset_name in list(h5hashes.keys()):
-			dsets[dataset_name] = h5hashes[dataset_name]
-			
-		for dataset_name in list(h5matrix.h5f.keys()):
-			if dataset_name == 'fam':
-				matrixdsets[dataset_name] = h5matrix.h5f[dataset_name]
-			else:
-				matrixdsets[dataset_name] = h5matrix[dataset_name]
 
 		
 		#temporary, 
