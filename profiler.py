455a39182c7ebb9b40ce3cb6b6023db597123344
Unknown <laurent.kilchoer@epfl.ch>
2018-05-24 17:18:02 +0200

    modif dataframe save

--- profiler.py
+++ profiler.py
@@ -30,6 +30,8 @@ class Profiler:
         self.lsh = None
         self.hashes = None
 
+        self.go_terms_dict = {}
+
     def go_benchmarking_init(self, obo_file_path, gaf_file_path):
         self.go = obo_parser.GODag(obo_file_path)
         self.associations = read_gaf(gaf_file_path)
@@ -53,6 +55,7 @@ class Profiler:
             fam_id = hashutils.hogid2fam(hog_id)
 
         # query_hashes dict keys:lminhashname, values:hashes
+        # get it from h5hashes instead of recomputing it
         query_hashes = hashutils.fam2hashes(fam_id, self.db_obj, self.tree, self.replacement_dic, events, combination)
 
         query_dict = {}
@@ -69,47 +72,62 @@ class Profiler:
         results = self.hog_query(hog_id=hog_id, fam_id=fam_id, events=events, combination=combination)
         for k, v in results.items():
             print('{} queries found for {}'.format(len(v), k))
+            print(v)
         if not scores:
             results_df = pd.DataFrame.from_dict(results)
             return results_df
 
-        validation_dict = {}
+        validation_list = []
 
         for query, results_list in results.items():
+            if 'duplication' not in query and 'presence' in query:
+                validation_dict = {}
+                print('getting score for {}'.format(query))
 
-            for result in results_list:
-                validation_dict[(query, result)] = self.compute_jaccard_distance_query(query, result)
+                validation_dict.update(self.compute_jaccard_distance_query(query, results_list, validation_dict, events))
 
-        validation_df = pd.DataFrame.from_dict(validation_dict, orient='index')
+                validation_dict.update(self.compute_semantic_distance_query(results_list, validation_dict, events, query))
 
-        return validation_df
+                validation_df = pd.DataFrame.from_dict(validation_dict, orient='index')
+                validation_df['event'] = query
+
+                print(validation_df)
+
+                validation_list.append(validation_df)
+
+        return validation_list
 
     def results(self, hog_id=None, fam_id=None, events=['duplication', 'gain', 'loss', 'presence'],
                 combination=True, scores=False):
-
         results = self.hog_query(hog_id=hog_id, fam_id=fam_id, events=events, combination=combination)
         for k, v in results.items():
             print('{} queries found for {}'.format(len(v), k))
+            print(v)
         if not scores:
             results_df = pd.DataFrame.from_dict(results)
             return results_df
 
-        result_dict = {}
+        dataframe_list = []
 
         # semantic scores
         for query, result in results.items():
+            result_dict = {}
             print('computing scores for query {}'.format({query}))
 
             related_hog_list = list(set([query] + [r for r in result]))
             events_combo = hashutils.result2events(query)
 
-            result_dict = self.compute_semantic_distance(related_hog_list, result_dict, events_combo)
+            result_dict.update(self.compute_semantic_distance(related_hog_list, result_dict, events_combo))
 
             # get jaccard score, here its possible to use mp
-            result_dict = self.compute_jaccard_distance(related_hog_list, result_dict, events_combo)
+            jaccard = self.compute_jaccard_distance(related_hog_list, result_dict, events_combo)
+            for index in jaccard:
+                result_dict[index].update(jaccard[index])
 
-        results_df = pd.DataFrame.from_dict(result_dict, orient='index')
-        return results_df
+            results_df = pd.DataFrame.from_dict(result_dict, orient='index')
+            dataframe_list.append(results_df)
+
+        return dataframe_list
 
     def compute_semantic_distance(self, hogs_list, result_dict, events_combo):
 
@@ -118,8 +136,30 @@ class Profiler:
             for j, hog_event_2 in enumerate(hogs_list):
                 hog1 = hashutils.result2hogid(hog_event_1)
                 hog2 = hashutils.result2hogid(hog_event_2)
+
+
                 semantic_dist = self.goTermAnalysis.semantic_similarity_score(hog1, hog2)
-                result_dict[(hog1, hog2, events_combo_string, 'semantic_distance')] = semantic_dist
+                result_dict[(hog1, hog2)] = {'Semantic': semantic_dist}
+
+        return result_dict
+
+    def compute_semantic_distance_query(self, hogs_list, result_dict, events_combo, query):
+
+        hog_query = hashutils.result2hogid(query)
+
+        if hog_query not in self.go_terms_dict:
+            self.go_terms_dict[hog_query] = self.goTermAnalysis.get_go_terms(hog_query)
+            print(self.go_terms_dict)
+
+        for i, hog_event in enumerate(hogs_list):
+            hog_other = hashutils.result2hogid(hog_event)
+
+            if hog_other not in self.go_terms_dict:
+                self.go_terms_dict[hog_other] = self.goTermAnalysis.get_go_terms(hog_other)
+
+            semantic_dist = self.goTermAnalysis.semantic_similarity_score_from_go_terms(self.go_terms_dict[hog_query],
+                                                                                        self.go_terms_dict[hog_other])
+            result_dict[(hog_query, hog_other)]['Semantic'] = semantic_dist
 
         return result_dict
 
@@ -129,25 +169,30 @@ class Profiler:
 
         with h5py.File(self.hashes, 'r') as h5hashes:
             query_hash = {query: hashutils.fam2hash_hdf5(hashutils.result2fam(query), h5hashes, events_combo)}
+
             results_hashes = {result: hashutils.fam2hash_hdf5(hashutils.result2fam(result), h5hashes, events_combo)
                               for result in results_list}
 
-        hash_compare_query = [((0, query_hash), (i, result_hash)) for i, result_hash in enumerate(results_hashes)]
+        print(results_hashes)
 
-        pool = mp.Pool(10)
+        hash_compare_query = [((0, query_hash[query]), (i, results_hashes[h])) for i, h in enumerate(results_hashes)]
+        print(hash_compare_query)
+        #pool = mp.Pool(10)
+        print('before pool')
+        #jaccard_results = pool.map_async(jaccard_mp, hash_compare_query, 10).get(9999999)
 
-        jaccard_results = pool.map_async(jaccard_mp, hash_compare_query, 10).get()
+        jaccard_results = list(map(jaccard_mp, hash_compare_query))
 
         # feed matrix and big dict with results
         for jac_res in jaccard_results:
-
+            print('getting results')
             i, j, jac_dist = jac_res
             hog1 = hashutils.result2hogid(query)
             hog2 = hashutils.result2hogid(results_list[j])
 
-            result_dict[(hog1, hog2, events_combo_string, 'jaccard_distance')] = jac_dist
+            result_dict[(hog1, hog2)] = {'Jaccard': jac_dist}
 
-        pool.close()
+        #pool.close()
 
         return result_dict
 
@@ -159,6 +204,7 @@ class Profiler:
 
             hashes = {result: hashutils.fam2hash_hdf5(hashutils.result2fam(result), h5hashes, events_combo)
                       for result in hogs_list}
+
 
         hash_compare = [((h1[0], hashes[h1[1]]), (h2[0], hashes[h2[1]]))
                         for h1, h2 in itertools.combinations(enumerate(hashes), 2)]
@@ -174,7 +220,7 @@ class Profiler:
             hog1 = hashutils.result2hogid(hogs_list[i])
             hog2 = hashutils.result2hogid(hogs_list[j])
 
-            result_dict[(hog1, hog2, events_combo_string, 'jaccard_distance')] = jac_dist
+            result_dict[(hog1, hog2)] = {'Jaccard': jac_dist}
 
         pool.close()
 
@@ -186,7 +232,9 @@ class Profiler:
         df_results = self.results(hog_id=hog_id, fam_id=fam_id, events=events, combination=combination, scores=scores)
         print('saving ...')
         if path_to_save is not None:
-            df_results.to_csv(path_to_save, sep='\t')
+
+            concat_result = pd.concat(list(df_results))
+            concat_result.to_csv(path_to_save, sep='\t')
             print(df_results)
         print('done')
 
@@ -198,7 +246,8 @@ class Profiler:
                                   scores=scores)
         print('saving ...')
         if path_to_save is not None:
-            df_results.to_csv(path_to_save, sep='\t')
+            concat_validation = pd.concat(df_results)
+            concat_validation.to_csv(path_to_save, sep='\t')
             print(df_results)
         print('done')
 
--- run_profiler.py
+++ run_profiler.py
@@ -17,7 +17,11 @@ import profiler
 from utils import config_utils
 profiler = profiler.Profiler(config_utils.omadir + 'OmaServer.h5')
 profiler.go_benchmarking_init(config_utils.datadirLaurent + 'project/data/go.obo', config_utils.datadirLaurent + 'project/data/gene_association.tair')
-profiler.lsh_loader(config_utils.datadirLaurent + 'May_23_2018_09_28_0.9_newlsh.pkl', config_utils.datadirLaurent + 'May_23_2018_13_27hashes.h5')
+profiler.lsh_loader(config_utils.datadirLaurent + 'May_24_2018_14_00_0.7_newlsh.pkl', config_utils.datadirLaurent + 'May_24_2018_14_00hashes.h5')
+
+
 profiler.results_save(fam_id=399563, scores=True, path_to_save=config_utils.datadirLaurent + 'test_01')
+
+profiler.validation_save(fam_id=399563, scores=True, path_to_save=config_utils.datadirLaurent + 'test_02')
 
 
