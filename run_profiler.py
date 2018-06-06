0a8fc6143e29d5572ceef761edcd35c53f8dd619
Unknown <laurent.kilchoer@epfl.ch>
2018-06-06 17:16:40 +0200

    validation

--- run_profiler.py
+++ run_profiler.py
@@ -1,25 +1,11 @@
-import profiler
-from utils import config_utils
-
-if __name__ == '__main__':
-
-    profiler = profiler.Profiler(config_utils.omadir + 'OmaServer.h5')
-    profiler.go_benchmarking_init(config_utils.datadirLaurent + 'project/data/go.obo', config_utils.datadirLaurent + 'project/data/gene_association.tair', config_utils.datadirLaurent + 'project/data/parents.h5',)
-
-    profiler.lsh_loader(config_utils.datadirLaurent + 'May_16_2018_17_33_0.7_newlsh.pkl')
-
-    profiler.results_save(fam_id=789, scores=True, path_to_save=config_utils.datadirLaurent + 'test_01')
-
-
 %load_ext autoreload
 %autoreload 2
 import profiler
 from utils import config_utils
-profilerObj = profiler.Profiler(config_utils.omadir + 'OmaServer.h5')
-profilerObj.go_benchmarking_init(config_utils.datadirLaurent + 'project/data/go.obo', config_utils.datadirLaurent + 'project/data/gene_association.tair', config_utils.datadirLaurent + 'project/data/parents.h5')
-profilerObj.lsh_loader(config_utils.datadirLaurent + 'May_23_2018_18_29_0.7_newlsh.pkl', config_utils.datadirLaurent + 'May_23_2018_18_29hashes.h5')
+from datetime import datetime
+date_string = "{:%B_%d_%Y_%H_%M}".format(datetime.now())
+profilerObj = profiler.Profiler(lsh_path=config_utils.datadirLaurent + 'June_05_2018_14_31_0.7_newlsh.pkl', hashes_path=config_utils.datadirLaurent + 'June_05_2018_14_31hashes.h5', obo_file_path=config_utils.datadirLaurent + 'project/data/go.obo', gaf_file_path=config_utils.datadirLaurent + 'project/data/gene_association.tair',h5_go_terms_parents_path=config_utils.datadirLaurent + 'project/data/parents.h5')
+profilerObj.validate_pipeline(path_to_save=config_utils.datadirLaurent + 'validation' + date_string + '.txt')
 
-
-profilerObj.save_results(fam_id=589123, path_to_save=config_utils.datadirLaurent + 'test_06.txt')
 
 
