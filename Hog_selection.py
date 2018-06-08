d4b3960a69d6a67e24e8666c800110e7904b437d
Unknown <laurent.kilchoer@epfl.ch>
2018-06-08 14:59:13 +0200

    validation cont

--- Hog_selection.py
+++ Hog_selection.py
@@ -31,37 +31,30 @@ def get_hogs_with_annotations(hogs):
             if json.loads(goterms):
                 hogs_with_annotations.append(hashutils.fam2hogid(fam))
 
-
     print(len(hogs_with_annotations))
     return hogs_with_annotations
 start_time = time()
 hogs_w_annotations = get_hogs_with_annotations(hogs2goterms)
 print('time to get hogs: {}'.format(time()-start_time))
 
-def fill_dict(dict, hog):
-    dict[hog] = goTermAnalysis.semantic_similarity_score(hog, hog)
 
+def make_dict(hog):
+    return {hog:goTermAnalysis.semantic_similarity_score(hog, hog)}
 
-#for hog in hogs_w_annotations:
-#    hogs_semantic_score[hog] = goTermAnalysis.semantic_similarity_score(hashutils.fam2hogid(hog), hashutils.fam2hogid(hog))
 
 start_time = time()
 if __name__ == '__main__':
 
-    # mgr = mp.Manager()
-    # hogs_semantic_score = mgr.dict()
-    # job = [mp.Process(target=fill_dict, args=(hogs_semantic_score, hog)) for hog in hogs_w_annotations]
-    # _ = [p.start() for p in job]
-    # _ = [p.join() for p in job]
-    hogs_semantic_score = {}
+    pool = mp.Pool()
+    results = pool.map_async(make_dict, hogs_w_annotations, 10*mp.cpu_count())
 
-    for hog in hogs_w_annotations:
-        fill_dict(hogs_semantic_score,hog)
+    results = results.get()
 
+    global_dict = {}
+    for r in results:
+        global_dict.update(r)
 
-    print(hogs_semantic_score)
-
-    dt = pd.DataFrame.from_dict(hogs_semantic_score, orient='index')
+    dt = pd.DataFrame.from_dict(global_dict, orient='index')
     dt.to_csv('hogvshog.csv', sep='\t')
 
 print(time()-start_time)
