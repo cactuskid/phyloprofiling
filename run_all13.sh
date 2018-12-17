5bca037cd595c860808799487fc7d822bc01cae7
cactuskid <cactuskid13@gmail.com>
2018-12-17 15:34:45 +0100

    del junk

--- run_all13.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-#BSUB -L /bin/bash
-
-#BSUB -o BOout2.txt -o:	standard	output
-#BSUB -e BOerror2.txt
-
-pyenv activate myenv
-python lshoptimizer.py  --dir /scratch/cluster/monthly/dmoi/profiling/run_all2/ --db all
